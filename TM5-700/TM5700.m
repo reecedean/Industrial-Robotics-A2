f5a70c824d7e4f4206c27e9b4b5d112732ff4dd7
jackc <jackc@DESKTOP-1U2ILRM>
2023-09-26 07:53:38 +1000

    update

--- TM5-700/TM5700.m
+++ TM5-700/TM5700.m
@@ -1,25 +1,21 @@
-classdef TM5700 < handle
+classdef TM5700 < RobotBaseClass
     %% TM5-700 robot
 
     properties(Access = public)              
-        stlFileNameStem = 'TM5-700'; % .ply files pulled from LinearUR5 & UR3
+        plyFileNameStem = 'TM5-700'; % .ply files pulled from LinearUR5 & UR3
     end
     
     methods
-        %% Constructor
-        function self = TM5700(baseTr)
-            disp('Starting');
-            self.CreateModel();
+ %% Define robot Function 
+        function self = LinearUR3(baseTr) 
+			self.CreateModel();
             if nargin < 1			
-				baseTr = eye(4)
-                
+				baseTr = transl(0,0,0.5); 			
             end
             self.model.base = self.model.base.T * baseTr * trotx(pi/2) * troty(pi/2);
-            disp('plotting');
-            self.PlotAndColourRobot();    
-            disp('finished plotting')
+            
+            self.PlotAndColourRobot();         
         end
-           
 
 %% Create the robot model
         function CreateModel(self)   
@@ -31,8 +27,8 @@ classdef TM5700 < handle
             link(5) = Link('d',0.106,'a',0,'alpha',pi/2,'qlim',deg2rad([-180,180]), 'offset',0);
             link(6) = Link('d',0.1144,'a',0,'alpha',0,'qlim',deg2rad([-270,270]), 'offset', 0);
             
-            self.model = SerialLink(link);
-            disp('created model')
+            self.model = SerialLink(link,'name',self.name);
+
         end
     end
 end
