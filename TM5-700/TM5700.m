d9bcf6a108b5d96ff9e3f5ca172b31816320bd8b
jackc <jackc@DESKTOP-1U2ILRM>
2023-09-26 21:36:33 +1000

    k

--- TM5-700/TM5700.m
+++ TM5-700/TM5700.m
@@ -1,44 +1,27 @@
-classdef TM5700 < handle
+classdef TM5700 < RobotBaseClass
     %% TM5-700 robot
 
     properties(Access = public)              
-        % plyFileNameStem = 'LinearUR3'; % .ply files pulled from LinearUR5 & UR3
+        plyFileNameStem = 'TM5700'; % .ply files pulled from LinearUR5 & UR3
     end
     
     methods
-        %% Constructor
-        function self = TM5700(baseTr,useTool,toolFilename)
-            if nargin < 3
-                if nargin == 2
-                    error('If you set useTool you must pass in the toolFilename as well');
-                elseif nargin == 0 % Nothing passed
-                    baseTr = transl(0,0,0);  
-                end             
-            else % All passed in 
-                self.useTool = useTool;
-                toolTrData = load([toolFilename,'.mat']);
-                self.toolTr = toolTrData.tool;
-                self.toolFilename = [toolFilename,'.ply'];
+ %% Define robot Function 
+ function self = TM5700(baseTr) 
+			self.CreateModel();
+            q0 = [0,0,0,0,0,0]
+            if nargin < 1			
+				baseTr = transl(0,0,0.5); 			
             end
-<<<<<<< HEAD
-          
-            self.CreateModel();
-			self.model.base = self.model.base.T * baseTr;
-            self.model.tool = self.toolTr;
-            self.PlotAndColourRobot();
-=======
             self.model.base = self.model.base.T * baseTr * trotx(pi/2) * troty(pi/2);
             self.PlotAndColourRobot(); 
         end
->>>>>>> 20af6bd013a8f9b733c0b03cdfc21a893d6a53de
 
-            drawnow
-        end
 %% Create the robot model
         function CreateModel(self)   
             % Create the TM5-700 model 
             link(1) = Link('d',0.1451,'a',0,'alpha',-pi/2,'qlim',deg2rad([-270 270]), 'offset',0);
-            link(2) = Link('d',0,'a',0329,'alpha',0,'qlim', deg2rad([-180 180]), 'offset',-pi/2);
+            link(2) = Link('d',0,'a',0.329,'alpha',0,'qlim', deg2rad([-180 180]), 'offset',-pi/2);
             link(3) = Link('d',0,'a',0.3115,'alpha',0,'qlim', deg2rad([-155 155]), 'offset', 0);
             link(4) = Link('d',-0.1222,'a',0,'alpha',pi/2,'qlim',deg2rad([-180 180]),'offset', 0);
             link(5) = Link('d',0.106,'a',0,'alpha',pi/2,'qlim',deg2rad([-180,180]), 'offset',0);
