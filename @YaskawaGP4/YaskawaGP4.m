1f417e63730a6ae994f99ce150b97c4189d1a0df
Jared <Jared@DESKTOP-3GQJJR9.modem>
2023-10-10 15:25:41 +1100

    Revised Working GP4 Files

--- @YaskawaGP4/YaskawaGP4.m
+++ @YaskawaGP4/YaskawaGP4.m
@@ -10,7 +10,7 @@ classdef YaskawaGP4 < RobotBaseClass
         function self = YaskawaGP4(baseTr)
             self.CreateModel();
             if nargin < 1
-                baseTr = transl(0.8,0.5,0.8)
+                baseTr = eye(4)
 
             end
             self.model.base = self.model.base.T * baseTr;
@@ -22,23 +22,20 @@ classdef YaskawaGP4 < RobotBaseClass
         %% Create the robot model
         function CreateModel(self)
             % Create the UR3 model mounted on a linear rail
-            link(1) = Link('d',0.165,'a',0,'alpha', 0, 'offset',0); % 165mm from the base TR
-            link(2) = Link('d',0.165,'a',0,'alpha', 0, 'offset',0); % 165mm from the joint 1 TR
-            % 99.45 + 131.1/2
-            link(3) = Link('d',0.260,'a',0,'alpha', 0, 'offset',0);
-            link(4) = Link('d',0.015,'a',-0.0955,'alpha', 0, 'offset',0); 
-            link(5) = Link('d',0,'a',-0.1945,'alpha', 0, 'offset',0); % .1945
-            link(6) = Link('d',0,'a',-0.0625,'alpha', 0, 'offset',0); % .0625
-            % link(5) = Link([0      0.11235   0         pi/2    0]);
-            % link(6) = Link([0      0.08535   0         -pi/2   0]);
+            link(1) = Link('d',0.330,'a',0,'alpha', pi/2, 'offset',0); % 165mm from the base TR
+            link(2) = Link('d',0,'a',0.260,'alpha', 0, 'offset',pi/2); % 165mm from the joint 1 TR
+            link(3) = Link('d',0,'a',-0.015,'alpha', -pi/2, 'offset',pi);
+            link(4) = Link('d',-0.290,'a',0,'alpha', -pi/2, 'offset',0); 
+            link(5) = Link('d',0,'a', 0,'alpha', pi/2, 'offset', 0); % .1945
+            link(6) = Link('d',-0.072,'a', 0,'alpha', pi, 'offset',pi); % .0625
 
             % Incorporate joint limits
             link(1).qlim = [-170 170]*pi/180;
             link(2).qlim = [-110 130]*pi/180;
-            link(3).qlim = [-65 200]*pi/180;
+            link(3).qlim = [-200 65]*pi/180; %% qlinm reversed to work properly
             link(4).qlim = [-200 200]*pi/180;
             link(5).qlim = [-123 123]*pi/180;
-            link(6).qlim = [-455 4555]*pi/180;
+            link(6).qlim = [-455 455]*pi/180;
 
             self.model = SerialLink(link,'name',self.name);
             
