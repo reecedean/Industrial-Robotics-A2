f00789f9bce96f0e5d406fcc0f368c687fe7cc2e
reecedean <reece.dean@student.uts.edu.au>
2023-10-12 12:21:47 +1100

    Added Cup File

---
+++ Cup/Cup.m
@@ -0,0 +1,25 @@
+classdef Cup < RobotBaseClass
+%% LinearUR3 UR3 on a non-standard linear rail created by a student
+
+    properties(Access = public)              
+        plyFileNameStem = 'Cup'; 
+    end
+    
+    methods
+%% Define robot Function 
+        function self = Cup(baseTr) 
+			self.CreateModel();
+            if nargin < 1			
+				baseTr = transl(0,0,0); 			
+            end
+            self.model.base = self.model.base.T * baseTr * trotx(pi/2) * troty(pi/2);
+            self.PlotAndColourRobot();
+        end
+        
+%% Create the Cup model
+    function CreateModel(self)
+        link(1) = Link('alpha', 0, 'a', 0, 'd', 0);
+        self.model = SerialLink(link,'name',self.name);
+    end
+    end
+end
