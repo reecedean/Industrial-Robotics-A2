6459671b95492af2094648179798f60c6092851c
Jared <Jared@DESKTOP-3GQJJR9.modem>
2023-10-25 13:34:22 +1100

    Cafe Environment with some colours. Done silver for benches but have realised it doesn't look good in matlab

--- CafeEnvironment.m
+++ CafeEnvironment.m
@@ -3,12 +3,56 @@ classdef CafeEnvironment < handle
         tableHandle % Store a handle to the table object
     end
     methods
-        function self = CafeEnvironment()
-            self.placeTable()
+        function main = CafeEnvironment()
+            axis("equal")
+            main.placeBartop;
+            main.placeTables;
+            main.placeShelves;
+            main.placeGate;
+            main.placeEmergencyStop;
+            main.placeCoffeeMachine;
+            main.placeWalls;
         end
         
-        function placeTable(self)
-            self.tableHandle = PlaceObject('Assignment2_ws3.ply', [0,0,0;0,0,0]);
+        function placeBartop(main)
+            hold on;
+            bartop = PlaceObject('Ply Files/Bartop_Silver_Navy.ply', [0,0,0;0,0,0]);
+        end
+
+        function placeTables(main)
+            table1 = PlaceObject('Ply Files/Table_Silver.ply', [0,0,0;0,0,0]);
+            table2 = PlaceObject('Ply Files/Table2_Silver.ply', [0,0,0;0,0,0]);
+        end
+
+        function placeShelves(main)
+            shelf1 = PlaceObject('Ply Files/Shelf_grey.ply', [0,0,0;0,0,0]);
+        end
+
+        function placeGate(main)
+            gate = PlaceObject('Ply Files/Gate_Black.ply', [0,0,0;0,0,0]);
+        end
+
+        function placeEmergencyStop(main)
+            eStop = PlaceObject('Ply Files/Emergency_Stop.ply', [0,0,0;0,0,0]);
+        end
+
+        function placeCoffeeMachine(main)
+            CoffeeMachine = PlaceObject('Ply Files/Coffee_Machine.ply', [0,0,0;0,0,0]);
+        end
+
+        function placeWalls(main)
+            surf([-3,-3;1.5,1.5] ...
+                ,[-2.5, 1.6; -2.5, 1.6] ...
+                ,[0.01,0.01;0.01,0.01] ...
+                ,'CData',imread('concrete.jpg') ...
+                ,'FaceColor','texturemap');
+
+            surf([1.5,1.5; 1.5,1.5] ...
+                ,[-2.5, 1.6; -2.5, 1.6] ...
+                ,[0.01, 0.01;2.5,2.5] ...
+                ,'CData',imread('brick_wall_black.jpg') ...
+                ,'FaceColor','texturemap');
+
         end
     end
 end
